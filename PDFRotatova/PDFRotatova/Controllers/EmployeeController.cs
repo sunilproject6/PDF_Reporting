59479592cad333df5c438cbad00ff25c5446e176
sunil <sunilproject6@gmail.com>
2019-07-02 14:51:38 +0530

    Project created poc of report

---
+++ PDFRotatova/PDFRotatova/Controllers/EmployeeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelProject;
+using System.ComponentModel.DataAnnotations;
+using RepositoryProject;
+namespace PDFRotatova.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private IEmployeeRepostory _bookRepository;
+        public EmployeeController()
+        {
+            this._bookRepository = new EmployeeRepository();
+        }
+
+        /// <summary>  
+        /// Print Employees details  
+        /// </summary>  
+        /// <returns></returns>  
+        public ActionResult PrintAllEmployee()
+        {
+            var report = new Rotativa.ActionAsPdf("Employees");
+            return report;
+        }
+        public ActionResult Employees()
+        {
+            return View(LoadEmployees());
+        }
+
+        [NonAction]
+        /// <summary>  
+        /// Load all Employees  
+        /// </summary>  
+        /// <returns></returns>  
+        private List<EmployeeModel> LoadEmployees()
+        {
+
+            return _bookRepository.GetEmployees().ToList();
+        }
+
+        [HttpGet]
+        public ActionResult AddEmployee()
+        {
+            var employee = new EmployeeModel();
+            return View(employee);
+        }
+
+        [HttpPost]
+        public ActionResult AddEmployee(EmployeeModel employee)
+        {
+            var dbcntxt = new EmployeeDBContext();
+            dbcntxt.Employees.Add(employee);
+            dbcntxt.SaveChanges();
+            return RedirectToAction("Employees");
+        }
+}
+}
---
+++ PDFRotatova/RepositoryProject/EmployeeRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelProject;
+
+namespace RepositoryProject
+{
+   public  class EmployeeRepository:IEmployeeRepostory
+    {
+        public IEnumerable<EmployeeModel> GetEmployees()
+        {
+            var dbcntxt = new EmployeeDBContext();
+            var lstemployee = dbcntxt.Employees;
+            return lstemployee.ToList();
+        }
+    }
+}
